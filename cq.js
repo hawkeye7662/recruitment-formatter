6a23427ae86a929e58e704a930921593c9738743
hawkeye7662 <82515230+hawkeye7662@users.noreply.github.com>
2022-03-21 00:35:46 +0530

    Add files via upload

---
+++ cq.js
@@ -0,0 +1,45 @@
+const inputTextArea = document.getElementById('inputTextArea'),
+  outputTextArea = document.getElementById('outputTextArea'),
+  submit = document.querySelector('#submit'),
+  missingParameters = document.querySelector('#missingParams');
+
+submit.addEventListener('click', async (e) => {
+  e.preventDefault();
+  outputTextArea.value = '';
+  missingParameters.innerText = '';
+  const input = inputTextArea.value;
+  let arrayOfErrors = [];
+  clanNameRegex = /Clan Name/gi;
+  languageRegex = /Language/gi;
+  leagueRegex = /League/gi;
+  requiredStarsRegex = /Required Stars/gi;
+  typeRegex = /Type?/gi;
+  if (!input.match(clanNameRegex)) {
+    arrayOfErrors.push('Clan Name');
+  }
+  if (!input.match(languageRegex)) {
+    arrayOfErrors.push('Language');
+  }
+  if (!input.match(leagueRegex)) {
+    arrayOfErrors.push('League');
+  }
+  if (!input.match(requiredStarsRegex)) {
+    arrayOfErrors.push('Required Stars');
+  }
+  if (!input.match(typeRegex)) {
+    arrayOfErrors.push('Type');
+  }
+
+  if (arrayOfErrors.length != 0) {
+    missingParameters.parentElement.removeAttribute('hidden');
+    missingParameters.innerText += arrayOfErrors.join(',');
+    multipleSpacesRegex = /\s{4,}/g;
+    outputTextArea.value =
+      input.replaceAll(multipleSpacesRegex, '\n') +
+      '\n' +
+      arrayOfErrors.join(':\n') +
+      ':';
+  } else {
+    outputTextArea.value = 'Post matches format!';
+  }
+});
---
+++ ed.js
@@ -0,0 +1,41 @@
+const inputTextArea = document.getElementById('inputTextArea'),
+  outputTextArea = document.getElementById('outputTextArea'),
+  submit = document.querySelector('#submit'),
+  missingParameters = document.querySelector('#missingParams');
+
+submit.addEventListener('click', async (e) => {
+  e.preventDefault();
+  outputTextArea.value = '';
+  missingParameters.innerText = '';
+  const input = inputTextArea.value;
+  let arrayOfErrors = [];
+  valleyNameRegex = /Valley Name/gi;
+  memberRegex = /Members?/gi;
+  codeRegex = /Codes?/gi;
+  levelRegex = /Level/gi;
+  if (!input.match(valleyNameRegex)) {
+    arrayOfErrors.push('Valley Name');
+  }
+  if (!input.match(memberRegex)) {
+    arrayOfErrors.push('Members');
+  }
+  if (!input.match(codeRegex)) {
+    arrayOfErrors.push('Codes');
+  }
+  if (!input.match(levelRegex)) {
+    arrayOfErrors.push('Level');
+  }
+
+  if (arrayOfErrors.length != 0) {
+    missingParameters.parentElement.removeAttribute('hidden');
+    missingParameters.innerText += arrayOfErrors.join(',');
+    multipleSpacesRegex = /\s{4,}/g;
+    outputTextArea.value =
+      input.replaceAll(multipleSpacesRegex, '\n') +
+      '\n' +
+      arrayOfErrors.join(':\n') +
+      ':';
+  } else {
+    outputTextArea.value = 'Post matches format!';
+  }
+});
